82c6f658da409a086a78921bb17a5edb03833ebe
Thorsten Fischer <thorsten@froschi.org>
2012-06-12 17:37:34 +0200

    Initial import.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,11 @@
-packages = %w/
-  libpixman-1-0
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid", "precise"
+  packages |= %w/
+    libpixman-1-0
+  /
+end
 
 packages.each do |pkg|
   package pkg do
